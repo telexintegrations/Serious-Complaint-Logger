045079451778519c428a3d48c822f03abce21711
Georgia Nwa <georgia.u.nwa@gmail.com>
2025-02-22 18:26:28 +0100

    fix: spelling error

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
 const dotenv = require("dotenv");
 const { OpenAI } = require("openai");
-const { integrationSpecSettings } = require("/integrationSpec");
+const { integrationSpecSettings } = require("/integrationSpec.js");
 
 dotenv.config();
 
